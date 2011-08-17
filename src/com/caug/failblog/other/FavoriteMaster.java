16d9104ed790beb6f772e85551c4851903e53955
pinciotti.dan182@gmail.com <pinciotti.dan182@gmail.com@09bb6f3d-6362-c6cf-0271-d47801c2a3a4>
2011-08-17 19:50:21 +0000

    Finished creating the about screen and favorites list screen

---
+++ src/com/caug/failblog/other/FavoriteMaster.java
@@ -0,0 +1,16 @@
+package com.caug.failblog.other;
+
+public class FavoriteMaster
+{
+	private String name;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
