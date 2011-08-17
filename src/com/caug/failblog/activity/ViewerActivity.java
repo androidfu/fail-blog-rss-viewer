e240540eb68ed511434e26534c54d5c27bdadd5e
pinciotti.dan182@gmail.com <pinciotti.dan182@gmail.com@09bb6f3d-6362-c6cf-0271-d47801c2a3a4>
2011-08-17 21:25:57 +0000

    Fixed splash screen weirdness

--- src/com/caug/failblog/activity/ViewerActivity.java
+++ src/com/caug/failblog/activity/ViewerActivity.java
@@ -3,58 +3,18 @@ package com.caug.failblog.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Animation.AnimationListener;
-import android.widget.ImageView;
 
 public class ViewerActivity extends Activity 
 {
-	private static final int STOP_SPLASH = 0;
-	private static final long SPLASH_TIME = 3000;
-	
-	private ImageView splash;
-	private Animation fadeOut;
-	
 	public void onCreate(Bundle savedInstanceState) 
     {
 		super.onCreate(savedInstanceState);
 		
 		// Choose which layout xml to display to the user
 		setContentView(R.layout.viewer);
-		
-		splash = (ImageView) findViewById(R.id.splashscreen);
-		
-		// Setup the splash animation
-		fadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
-		fadeOut.setAnimationListener(new AnimationListener() {
-
-			public void onAnimationStart(Animation animation)
-			{
-				// Do nothing
-			}
-			
-			public void onAnimationRepeat(Animation animation)
-			{
-				// Do nothing
-			}
-			
-			public void onAnimationEnd(Animation animation)
-			{
-				// Hide the image
-				splash.setVisibility(View.GONE);
-			}
-		});
-		
-		Message msg = new Message();
-		msg.what = STOP_SPLASH;
-		splashHandler.sendMessageDelayed(msg, SPLASH_TIME);
     }
 	
 	protected void onResume()
@@ -97,22 +57,4 @@ public class ViewerActivity extends Activity
 		        return super.onOptionsItemSelected(item);
 	    }
 	}
-	
-	/*
-	 * This method will handle the messages sent relating to the splash screen
-	 */
-	private Handler splashHandler = new Handler()
-	{
-		public void handleMessage(Message msg)
-		{
-			switch(msg.what)
-			{
-				case STOP_SPLASH:
-					splash.startAnimation(fadeOut);
-					break;
-			}
-			
-			super.handleMessage(msg);
-		}
-	};
 }
