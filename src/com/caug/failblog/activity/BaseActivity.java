a0324be834710bb805a15e6048a0015a114e1226
mavorst@gmail.com <mavorst@gmail.com@09bb6f3d-6362-c6cf-0271-d47801c2a3a4>
2011-11-13 20:29:42 +0000

    Latest Failblog code including Signed APK

---
+++ src/com/caug/failblog/activity/BaseActivity.java
@@ -0,0 +1,49 @@
+package com.caug.failblog.activity;
+
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity 
+{
+	protected static GoogleAnalyticsTracker tracker;
+	
+	public BaseActivity() 
+	{
+		super();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		
+		if(tracker == null)
+		{
+			tracker = GoogleAnalyticsTracker.getInstance();
+	
+		    tracker.startNewSession("UA-26997352-1", this);
+		}
+	}
+	
+	protected void trackPageView(String page)
+	{
+		if(tracker == null)
+		{
+			tracker = GoogleAnalyticsTracker.getInstance();
+		    tracker.startNewSession("UA-26997352-1", this);		
+		}
+		tracker.trackPageView(page);
+	}
+	
+	protected void trackEvent(String categoty, String action, String label, int value)
+	{
+		if(tracker == null)
+		{
+			tracker = GoogleAnalyticsTracker.getInstance();
+		    tracker.startNewSession("UA-26997352-1", this);		
+		}
+		tracker.trackEvent(categoty, action, label, value); 
+	}
+}
